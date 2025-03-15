8733e4ee9cfc017f5e7267713d74d74403f97f79
Isma <IsmaaTR@users.noreply.github.com>
2025-03-15 14:54:20 +0100

    Refactoring

--- resources/js/game.js
+++ resources/js/game.js
@@ -6,7 +6,7 @@ const game = (function(gameBoard) {
     const WINNING_COMBINATIONS = [
         [0, 1, 2], // First row
         [3, 4, 5], // Second row
-        [6, 7, 8], // Thrid row
+        [6, 7, 8], // Third row
         [0, 3, 6], // First column
         [1, 4, 7], // Second column
         [2, 5, 8], // Third column
@@ -19,13 +19,13 @@ const game = (function(gameBoard) {
 
     let currentPlayer = player1;
 
-    init()
+    init();
 
     function init() {
-        //Add the event listeners to the cells in the DOM
+        // Add the event listeners to the cells in the DOM
         gameBoard.cellsDOM.forEach((cell, index) => {
             cell.addEventListener('click', () => playRound(index));
-        })
+        });
     }
 
     function playRound(index) {
@@ -33,21 +33,18 @@ const game = (function(gameBoard) {
         const roundResult = checkWinner(boardStatus);
         switch (roundResult) {
             case player1.mark:
-                currentPlayer = player1;
                 player1.addScore();
                 gameBoard.reset();
                 break;
             case player2.mark:
-                currentPlayer = player1;
                 player2.addScore();
                 gameBoard.reset();
                 break;
             case "draw":
-                currentPlayer = player1;
                 gameBoard.reset();
                 break;
             default:
-                currentPlayer === player1 ? currentPlayer = player2 : currentPlayer = player1;
+                currentPlayer = currentPlayer === player1 ? player2 : player1;
                 break;
         }
     }
@@ -55,16 +52,13 @@ const game = (function(gameBoard) {
     function checkWinner(board) {
         for (let combination of WINNING_COMBINATIONS) {
             const [a, b, c] = combination;
-            
             if (board[a] && board[a] === board[b] && board[a] === board[c]) {
                 return board[a]; // Returns the winner
             }
         }
-        
         if (!board.includes(null)) {
             return "draw"; // No empty spaces available
         }
-        
         return null; 
     }
 
